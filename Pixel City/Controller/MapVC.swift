b1354c562d206aeab039c10f6312f5db9737981c
Shreya Randive <shreyarandive28@gmail.com>
2019-07-16 15:59:16 -0400

    Setup the mapkit, write function to center view at the user location

--- Pixel City/Controller/MapVC.swift
+++ Pixel City/Controller/MapVC.swift
@@ -8,20 +8,48 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
     }
 
     @IBAction func mapBtnPressed(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMaponUserLocation()
+        }
     }
 }
 
 extension MapVC: MKMapViewDelegate {
+    func centerMaponUserLocation() {
+        guard let userLocationCoordinate = locationManager.location?.coordinate else { return }
+        let userLocationCoordinateRegion = MKCoordinateRegion.init(center: userLocationCoordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
+        mapView.setRegion(userLocationCoordinateRegion, animated: true)
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
     
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMaponUserLocation()
+    }
 }
 
