e9f2924ce5ea679750aee5900618eeac700cd8a3
Shreya Randive <shreyarandive28@gmail.com>
2019-07-16 14:13:18 -0400

    Initial commit

---
+++ Pixel City/Pixel City/Utilities/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pixel City
+//
+//  Created by Shreya Randive on 7/16/19.
+//  Copyright © 2019 Shreya Randive. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
